7c380ed27432a0fa3da830049e2259684cf7cf3d
manjuravi06 <manjuravi06@gmail.com>
2020-07-15 20:17:11 +0530

    Asian Artitect Changes

--- src/jira/api/testing/HelloWorld.java
+++ src/jira/api/testing/HelloWorld.java
@@ -8,6 +8,10 @@ public class HelloWorld {
 		System.out.println("Hi this is a change in the program");
 
 		System.out.println("This is the thid change");
+
+		System.out.println("Adding changes to new branch develop");
+
+		System.out.println("Adding my secind change to develop branch");
 	}
 
 }
